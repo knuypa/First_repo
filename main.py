2f0feaf4ad00bf62c578d2d60bf529beac1bb499
knuypa <mikhail.knuypa@gmail.com>
2024-01-18 19:18:44 +0200

    git commit

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ print("Hallo Go IT!")
 name = "Mykhailo"
 second_name = "Knuipa"
 age = 30
+height = 1.64
+weight = 78.5
 city = "Bonn"
 
-print(name,second_name, age, city)
+print(name,second_name, age, height, weight, city)
