fbc90dd7cb2f0cdd59699d271d8648d463b53615
knuypa <mikhail.knuypa@gmail.com>
2024-01-26 15:51:10 +0200

    git commit

--- main.py
+++ main.py
@@ -14,6 +14,6 @@ weight = 78.5
 country = "Germany"
 city = "Bonn"
 street = "Brunsgasse"
-nummber == 31
+number == 31
 
-print(name,second_name, age, height, weight, country, city, street, nummber)
+print(name,second_name, age, height, weight, country, city, street, number)
